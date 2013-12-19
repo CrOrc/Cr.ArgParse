0ab77d0c6649de7df8c1eacc9448f3da6cb38290
Roman Yakubuk <crazyorcmeister@gmail.com>
2013-12-19 14:56:25 +0300

    Add single argument description

--- src/Cr.ArgParse/Argument.cs
+++ src/Cr.ArgParse/Argument.cs
@@ -1,7 +1,23 @@
-﻿namespace Cr.ArgParse
+﻿using System.Collections.Generic;
+
+namespace Cr.ArgParse
 {
+    /// <summary>
+    /// Single argument description
+    /// </summary>
     public class Argument
     {
-        
+        /// <summary>
+        /// strings to identify argument. Empty for positional arguments
+        /// </summary>
+        public IList<string> OptionStrings { get; set; }
+        /// <summary>
+        /// Name for storing parsing results
+        /// </summary>
+        public string Destination { get; set; }
+        /// <summary>
+        /// Description of argument
+        /// </summary>
+        public string HelpText { get; set; }
     }
 }
