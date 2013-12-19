7538240c3449f20259b9a9e8179b00ce89a99d5c
Roman Yakubuk <crazyorcmeister@gmail.com>
2013-12-19 19:55:38 +0300

    Started implementing argument parser

---
+++ src/Cr.ArgParse/ListExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Cr.ArgParse.Extensions
+{
+    public static class ListExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IList<T> list)
+        {
+            return ReferenceEquals(list, null) || list.Count < 1;
+        }
+    }
+}
