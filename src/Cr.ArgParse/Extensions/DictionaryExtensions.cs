1a1a530faf132ae9c4a374a415e6320d1c2ae0c2
Roman Yakubuk <crazyorcmeister@gmail.com>
2013-12-20 18:49:05 +0300

    Adding argument actions

---
+++ src/Cr.ArgParse/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cr.ArgParse.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static bool SafeContainsKey<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            if (dictionary == null || Equals(key, null)) return false;
+            return dictionary.ContainsKey(key);
+        }
+
+        public static TValue SafeGetValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key,
+            TValue defaultValue = default (TValue),
+            Func<TValue, bool> fallbackToDefaultPredicate = null)
+        {
+            if (dictionary == null || Equals(key, null)) return defaultValue;
+            TValue ret;
+            return dictionary.TryGetValue(key, out ret) &&
+                   !(fallbackToDefaultPredicate != null && fallbackToDefaultPredicate(ret))
+                ? ret
+                : defaultValue;
+        }
+
+        public static IDictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+            this IEnumerable<KeyValuePair<TKey, TValue>> dictionary,
+            Func<KeyValuePair<TKey, TValue>, bool> filterPredicate = null,
+            IEqualityComparer<TKey> equalityComparer = null)
+        {
+            if (dictionary == null) return null;
+
+            var res = new Dictionary<TKey, TValue>(equalityComparer);
+            var items = filterPredicate == null ? dictionary : dictionary.Where(filterPredicate);
+            foreach (var kv in items)
+                res[kv.Key] = kv.Value;
+            return res;
+        }
+
+        public static IDictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+            this IEnumerable<KeyValuePair<TKey, TValue>> dictionary,
+            IEqualityComparer<TKey> equalityComparer = null)
+        {
+            return ToDictionary(dictionary, null, equalityComparer);
+        }
+
+    }
+}
